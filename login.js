32fa6d3a88899b523aaa6070a4cccdc2e94c5cc5
shmewy <167490786+shmewy@users.noreply.github.com>
2024-05-15 15:41:37 -0400

    yay??

--- login.js
+++ login.js
@@ -2,16 +2,15 @@
 document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault();
     
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    const passcode = document.getElementById('password').value;
 
     // Replace with actual authentication logic
-    if (username === 'friendofshmewy' && password === 'shmewyisagoodfriend') {
-        alert('yayyyy thanks for beign a gud persun');
+    if (passcode === 'shmewyisagoodfriend') {
+        alert('Welcome Premium User');
         sessionStorage.setItem('levelOfAccess','noAds')
         window.location.replace('transform.html');
     } else {
-        alert('You will have a limited experience');
+        alert('Contact me for the Premium Passcode');
         sessionStorage.setItem('levelOfAccess', 'yesAds')
         window.location.replace('transform.html')
     }
