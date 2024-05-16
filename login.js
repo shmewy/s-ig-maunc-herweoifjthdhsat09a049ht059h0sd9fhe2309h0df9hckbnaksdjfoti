132125c467100f5853efdaabe7e79f3e8d06e721
shmewy <167490786+shmewy@users.noreply.github.com>
2024-05-15 21:26:38 -0400

    broh

--- login.js
+++ login.js
@@ -5,12 +5,12 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     const passcode = document.getElementById('password').value;
 
     // Replace with actual authentication logic
-    if (passcode === 'shmewyisagoodfriend') {
-        alert('Welcome Premium User');
+    if (passcode === 'MoonShineUser') {
+        alert('Welcome to Lunate, Premium User');
         sessionStorage.setItem('levelOfAccess','noAds')
         window.location.replace('transform.html');
     } else {
-        alert('Contact me for the Premium Passcode');
+        alert('Contact me for the Premium Pass');
         sessionStorage.setItem('levelOfAccess', 'yesAds')
         window.location.replace('transform.html')
     }
